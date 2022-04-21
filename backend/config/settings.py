32a2b1ddbc96b7a7e08d48b833bc38bf48e2439f
Mazen Ramadan <mazenramadan00@gmail.com>
2022-04-21 18:05:17 +0200

    hiding secret key

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,19 +11,21 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from environ import Env
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = Env()
+env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-22tmn72fz%wl!ica5$_k=c0na!x@4twlxb3tbu24&gex-4((w@'
+SECRET_KEY = 'MY_SCRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG'
 
 ALLOWED_HOSTS = []
 

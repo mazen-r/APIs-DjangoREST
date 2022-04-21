4dd308152b0713469ddddb65a1b8c5e56d406ef9
Mazen Ramadan <mazenramadan00@gmail.com>
2022-04-21 17:34:12 +0200

    product model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,14 @@
 from django.http import JsonResponse
+from products.models import Product
+
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "This is django Api Response"})
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    return JsonResponse({data})
     
